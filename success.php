9ce4e350d693ddfec1a218010614fe7841cb73fe
abdelrahman <gadoua50@gmail.com>
2022-10-15 00:08:07 +0200

    create the success page

---
+++ success.php
@@ -0,0 +1,13 @@
+<?php
+require_once "include/header.php";
+?>
+
+<div class="conatiner d-flex flex-column justify-content-center align-items-center" style="height: 75%;">
+  <div class="alert alert-success" role="alert">
+    Success Donation, Thank you very much
+  </div>
+</div>
+
+<?php
+require_once "include/footer.php";
+?>

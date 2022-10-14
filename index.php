c19ddd7993874301b62d79c2bc3d4dec765c8d82
abdelrahman <gadoua50@gmail.com>
2022-10-15 00:11:28 +0200

    create the index page

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require_once "include/header.php";
+?>
+
+<div class="conatiner d-flex flex-column justify-content-center align-items-center" style="height: 75%;">
+  <form action="/create-checkout-session.php" method="POST">
+    <button type="submit" class="btn btn-outline-success btn-lg">Donate Now</button>
+  </form>
+</div>
+
+
+<?php
+require_once "include/footer.php";
+?>

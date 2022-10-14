cf54662b1fe7db3db7dbb7955caf09d1e0c17dbc
abdelrahman <gadoua50@gmail.com>
2022-10-15 00:11:03 +0200

    create the cancel page

---
+++ cancel.php
@@ -0,0 +1,13 @@
+<?php
+require_once "include/header.php";
+?>
+
+<div class="conatiner d-flex flex-column justify-content-center align-items-center" style="height: 75%;">
+  <div class="alert alert-danger" role="alert">
+    Your donation is cancelled.
+  </div>
+</div>
+
+<?php
+require_once "include/footer.php";
+?>

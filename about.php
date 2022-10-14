17638045219465940314759ec4f695ae749b624a
abdelrahman <gadoua50@gmail.com>
2022-10-15 00:07:50 +0200

    create the about page

---
+++ about.php
@@ -0,0 +1,13 @@
+<?php
+require_once "include/header.php";
+?>
+
+<div class="conatiner d-flex flex-column justify-content-center align-items-center p-5" style="height: 75%;">
+  <h3>About the GRIP</h3>
+  <p class="container text-center">The Graduate Rotational Internship Program is a unique offer for students and recent graduates to experience and join The Sparks Foundation. In addition to skills-specific tasks, we encourage interns to build a credible professional profile.</p>
+  <p class="container text-center">This project is submitted as a task for THE GRIP Program</p>
+</div>
+
+<?php
+require_once "include/footer.php";
+?>
